0356700ac061d5b5a7489784cf25475c4fcb6c3a
Jackson Henry <jackson.henry13@yahoo.com>
2019-05-03 22:08:01 -0500

    Got the entry commands. argv[2] is for the user's actions. argv[3] is for the input Parameter.

--- liri.js
+++ liri.js
@@ -9,5 +9,12 @@ var request = require("request");
 //Require Spotify
 var spotify = require('node-spotify-api');
 
-var spotify = new Spotify(keys.spotify);
 
+//Declare the right commands. 
+//argv[2] = user's actions
+//argv[3] = input parameter(s)
+var userAction = process.argv[2];
+var inputParameter = process.argv[3];
+
+//Get the Spotify keys
+var spotify = new Spotify(keys.spotify);
