443265f4abc5688809520b25c6949b6160d6c64b
Jackson Henry <jackson.henry13@yahoo.com>
2019-05-03 21:47:27 -0500

    Getting the liri.js file set up. I have gotten the spotify in the file. Unfortunately, when I ran it I got a reference error.

--- liri.js
+++ liri.js
@@ -1,6 +1,10 @@
 //Read and set environment variables with the .env package
 require("dotenv").config();
 
+//All of the required vars
 var keys = require("./keys.js");
+
+var spotify = require('node-spotify-api');
+
 var spotify = new Spotify(keys.spotify);
 
